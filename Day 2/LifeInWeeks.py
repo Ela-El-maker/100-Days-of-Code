1840cb334615317e896ae1cea13ee75ac08c9f00
Ela-El-maker <feloela101@gmail.com>
2024-01-26 15:56:59 +0300

    Day 2 : Working with Formats

---
+++ Day 2/LifeInWeeks.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your current age : "))
+
+yearsRemaining = 90 - age
+daysRemaining = yearsRemaining *365
+weeksRemaining = yearsRemaining * 52
+monthsRemaining = yearsRemaining * 12
+
+print(f"You have {daysRemaining} days , {weeksRemaining} weeks and {monthsRemaining} months remaining to complete your request.")
---
+++ Day 2/typeCheckProject.py
@@ -0,0 +1,6 @@
+height = float(input('Enter your height in m : '))
+weight = int(input('Enter your weight in kg : '))
+
+bmi = weight/(height*height)
+bmi = int(weight/(height*height))
+print(bmi)
